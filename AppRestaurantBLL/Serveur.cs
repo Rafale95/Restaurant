8cb82d86b1df2cbf300ac0e4716b8bb3f9a19014
VORK Stéphane <vorksprogrammation@gmail.com>
2024-08-17 01:14:11 +0200

    Serveur basics

--- AppRestaurantBLL/Serveur.cs
+++ AppRestaurantBLL/Serveur.cs
@@ -3,7 +3,29 @@ using AppRestaurantDAL;
 
 namespace AppRestaurantBLL
 {
-    public class Serveur
+    public class ServeurBLL
     {
+
+        private ServeurDB serveurDB;
+
+        public ServeurBLL(ServeurDB serveurDB)
+        {
+            this.serveurDB = serveurDB;
+        }
+
+        public void CreateServeur(Serveur serveur)
+        {
+            serveurDB.CreateServeurDB(serveur);
+        }
+
+        public void EditServeur(Serveur serveur)
+        {
+            serveurDB.EditServeurDB(serveur);
+        }
+
+        public void DeleteServeur(Serveur serveur)
+        {
+            serveurDB.DeleteServeurDB(serveur);
+        }
     }
 }
--- AppRestaurantDAL/Serveur.cs
+++ AppRestaurantDAL/Serveur.cs
@@ -1,8 +1,29 @@
-﻿using System.Data.SqlClient;
+﻿using AppRestaurantBOL;
 
 namespace AppRestaurantDAL
 {
     public class ServeurDB
     {
+        private readonly string connectionString;
+
+        public ServeurDB(string ConnectionString)
+        {
+            connectionString = ConnectionString;
+        }
+
+        public void CreateServeurDB(Serveur serveur)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteServeurDB(Serveur serveur)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditServeurDB(Serveur serveur)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
