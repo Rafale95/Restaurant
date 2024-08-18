005a1ba793edd0ba5f00f4779043f380064aba71
VORK Stéphane <vorksprogrammation@gmail.com>
2024-08-18 12:24:45 +0200

    Restaurant.detail

--- AppRestaurant/Pages/Restaurants/Details.cshtml.cs
+++ AppRestaurant/Pages/Restaurants/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using AppRestaurantBLL;
+using AppRestaurantBOL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace AppRestaurant.Pages.Restaurants
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        public DetailsModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+
+        [BindProperty]
+        public Restaurant restaurant { get; set; }
+
+
+        public ActionResult OnGet(int id)
+        {
+            RestaurantBLL bll = new RestaurantBLL(_configuration.GetConnectionString(Program.CONNECTION_STRING));
+
+            if (id == null) return NotFound(); 
+
+            restaurant = bll.GetRestaurant(id);
+            return Page();
         }
     }
 }
